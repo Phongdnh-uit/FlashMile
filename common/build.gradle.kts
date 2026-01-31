d194cc991ff4b1bc43d4d1247df807c792cf7463
Phongdnh-uit <dangnguyenhuyphong@gmail.com>
2026-01-31 14:11:15 +0700

    🚧 build(server): base dto response

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,5 +1,8 @@
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
+	compileOnly("org.springframework.boot:spring-boot-starter-webmvc")
+	compileOnly("org.springframework.boot:spring-boot-starter-data-jpa")
+	compileOnly("org.springframework.boot:spring-boot-starter")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
