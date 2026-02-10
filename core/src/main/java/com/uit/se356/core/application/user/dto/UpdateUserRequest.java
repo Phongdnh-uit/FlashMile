f2205c2440a138856eaeb4e0e5e3ff6af57a473c
KhangPham205 <ptkhang17122005@gmail.com>
2026-02-10 20:57:58 +0700

    Update UpdateUserRequest.java

--- core/src/main/java/com/uit/se356/core/application/user/dto/UpdateUserRequest.java
+++ core/src/main/java/com/uit/se356/core/application/user/dto/UpdateUserRequest.java
@@ -1,7 +1,10 @@
 package com.uit.se356.core.application.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record UpdateUserRequest(
-        String fullName,
-        String email
+        @NotBlank String fullName,
+        @Email String email
 ) {
 }
